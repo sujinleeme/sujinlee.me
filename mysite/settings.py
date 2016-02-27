e1e8d341478dc684c0a18d7327a31665af9cc4bd
sujinleeme <sujinlee.me@gmail.com>
2016-02-28 01:26:47 +0900

    fixed typo in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,7 +129,7 @@ MARKDOWN_STYLE = '/static/css/markdown.css'
 MARKDOWN_EXTENSIONS = 'extra', 'codehilite'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'image')
