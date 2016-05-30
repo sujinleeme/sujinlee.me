9972cdb8b62787c1a7ac9f2dab90fcdc4c9abca2
sujinleeme <sujinlee.me@gmail.com>
2016-05-30 23:33:24 +0900

    edit share button url & javascript

--- myblog/static/js/script.js
+++ myblog/static/js/script.js
@@ -246,3 +246,32 @@ $(document).ready(function() {
         }, 300);
     })
 });
+
+
+/*socials*/
+;(function($){
+  $.fn.customerPopup = function (e, intWidth, intHeight, blnResize) {
+
+    // Prevent default anchor event
+    e.preventDefault();
+
+    // Set values for window
+    intWidth = intWidth || '500';
+    intHeight = intHeight || '400';
+    strResize = (blnResize ? 'yes' : 'no');
+
+    // Set title and open popup with focus on it
+    var strTitle = ((typeof this.attr('title') !== 'undefined') ? this.attr('title') : 'Social Share'),
+        strParam = 'width=' + intWidth + ',height=' + intHeight + ',resizable=' + strResize,
+        objWindow = window.open(this.attr('href'), strTitle, strParam).focus();
+  }
+
+  /* ================================================== */
+
+  $(document).ready(function ($) {
+    $('.customer.share').on("click", function(e) {
+      $(this).customerPopup(e);
+    });
+  });
+
+}(jQuery));
