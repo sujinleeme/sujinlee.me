2cd7369967a45c6b9b147bcd7b3bc45d18d88ec6
sujinleeme <sujinlee.me@gmail.com>
2016-05-30 17:00:01 +0900

    edit og tags individual templates

--- myblog/static/js/social.js
+++ myblog/static/js/social.js
@@ -41,3 +41,7 @@ window.fbAsyncInit = function() {
 }(document, 'script', 'facebook-jssdk'));
 
 /*- google plus -*/
+function googleshare() {
+    window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+    return false;
+}
