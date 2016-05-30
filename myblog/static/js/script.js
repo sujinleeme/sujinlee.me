28ce2ed40325be40e5cebd905cb368b0471e0860
sujinleeme <sujinlee.me@gmail.com>
2016-05-31 00:38:25 +0900

    edit social plugin

--- myblog/static/js/script.js
+++ myblog/static/js/script.js
@@ -246,37 +246,3 @@ $(document).ready(function() {
         }, 300);
     })
 });
-
-
-/*-- social popup UI --*/
-/*
-title: Add share popup to links
-author: Partrick Kahl
-available at: https://codepen.io/patrickkahl/pen/DxmfG
-*/
-;(function($){
-  $.fn.customerPopup = function (e, intWidth, intHeight, blnResize) {
-
-    // Prevent default anchor event
-    e.preventDefault();
-
-    // Set values for window
-    intWidth = intWidth || '500';
-    intHeight = intHeight || '400';
-    strResize = (blnResize ? 'yes' : 'no');
-
-    // Set title and open popup with focus on it
-    var strTitle = ((typeof this.attr('title') !== 'undefined') ? this.attr('title') : 'Social Share'),
-        strParam = 'width=' + intWidth + ',height=' + intHeight + ',resizable=' + strResize,
-        objWindow = window.open(this.attr('href'), strTitle, strParam).focus();
-  }
-
-  /* ================================================== */
-
-  $(document).ready(function ($) {
-    $('.customer.share').on("click", function(e) {
-      $(this).customerPopup(e);
-    });
-  });
-
-}(jQuery));
