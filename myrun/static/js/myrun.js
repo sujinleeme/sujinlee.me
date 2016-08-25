963a28075a72711bc7d981e8fedc21b7614ae538
sujinleeme <sujinlee.me@gmail.com>
2016-08-25 19:09:25 +0900

    add og author and facebook id

--- myrun/static/js/myrun.js
+++ myrun/static/js/myrun.js
@@ -1,3 +1,23 @@
+/*- facebook -*/
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '1081681621896558',
+        xfbml: true,
+        version: 'v2.3'
+    });
+};
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
 /* open new window */
 $(document).ready(function() {
     $(".NewTab").attr("target", "_blank");
