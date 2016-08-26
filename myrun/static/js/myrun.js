8f612d844ed965cc89aab0376d5fdc2458a93bb9
sujinleeme <sujinlee.me@gmail.com>
2016-08-26 18:54:28 +0900

    responsive web

--- myrun/static/js/myrun.js
+++ myrun/static/js/myrun.js
@@ -32,6 +32,21 @@ $(window).load(function() { // makes sure the whole site is loaded
   });
 })
 
+/* table */
+var headertext = [],
+headers = document.querySelectorAll("#event_title th"),
+tablerows = document.querySelectorAll("#event_title th"),
+tablebody = document.querySelector("#event tbody");
+
+for(var i = 0; i < headers.length; i++) {
+  var current = headers[i];
+  headertext.push(current.textContent.replace(/\r?\n|\r/,""));
+} 
+for (var i = 0, row; row = tablebody.rows[i]; i++) {
+  for (var j = 0, col; col = row.cells[j]; j++) {
+    col.setAttribute("data-th", headertext[j]);
+  } 
+}
 
 /*-- svg image --*/
 /*
