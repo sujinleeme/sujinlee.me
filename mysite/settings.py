fc7b90721bf557307a9ca614b2a1d5cd04087139
sujinleeme <sujinlee.me@gmail.com>
2016-10-21 00:55:48 +0900

    add SSL redirect

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django_markdown',
+    'tagging',
     'myblog',
     'myrun',
 ]
@@ -131,17 +132,21 @@ USE_TZ = True
 
 # site
 SITE_ID = 1
+
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
-
-# Markdown
-MARKDOWN_STYLE = '/static/css/markdown.css'
-
-MARKDOWN_EXTENSIONS = 'extra', 'codehilite'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'image')
+
+
+# Markdown
+MARKDOWN_STYLE = '/static/css/markdown.css'
+
+MARKDOWN_EXTENSIONS = 'extra', 'codehilite'
+
+#SSL
+SECURE_SSL_REDIRECT = True
